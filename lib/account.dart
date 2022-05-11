b64e3f8af9331b12d7cfaaf2cfa8f78d4e066e07
Rachid-Mek <78279178+Rachid-Mek@users.noreply.github.com>
2022-05-11 20:41:22 +0100

    Update account.dart

--- lib/account.dart
+++ lib/account.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class account extends StatefulWidget {
   account({Key? key}) : super(key: key);
@@ -11,7 +12,138 @@ class _accountState extends State<account> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('this is the account')),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: Text(
+          "Profile",
+          style: TextStyle(fontSize: 35, color: HexColor("#069A8E")),
+        ),
+        centerTitle: true,
+      ),
+      backgroundColor: HexColor("#EDE6DB"),
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                "images/user.png",
+                scale: 0.6,
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 20, horizontal: 5),
+                height: 320,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black,
+                      blurRadius: 2.0,
+                      spreadRadius: 0.0,
+                      offset: Offset(2.0, 2.0),
+                    ),
+                  ],
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Les informations personnels",
+                        style: TextStyle(
+                            color: HexColor("#069A8E"),
+                            fontSize: 26,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const Text(
+                        "Nom : MEKDOUD",
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      const Text(
+                        "Prenom : Rachid",
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      const Text(
+                        "Numéro de telephone:0561807602",
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      const Text(
+                        "Email :rachidmekdoud@gmail.com",
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      const Text(
+                        "Mot de passe: ●●●●●●●●●●●●",
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Modifier",
+                      style: TextStyle(fontSize: 25),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.all(10)),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Déconecter",
+                      style: TextStyle(fontSize: 25),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.all(10)),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
