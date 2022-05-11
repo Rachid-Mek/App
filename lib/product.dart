a9ef81bac6b375f90ca73f6bcf63a7527ad1f182
Rachid-Mek <78279178+Rachid-Mek@users.noreply.github.com>
2022-05-11 15:31:50 +0100

    Update product.dart

--- lib/product.dart
+++ lib/product.dart
@@ -1,45 +1,53 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 import 'medicament.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 class ProductCard extends StatelessWidget {
   final Product product;
+  final Function press;
 
-  const ProductCard({Key? key, required this.product}) : super(key: key);
+  const ProductCard({Key? key, required this.product, required this.press})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          padding: const EdgeInsets.all(2),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(12),
-            border: Border.all(
-              color: HexColor("#6EBF8B"),
-              width: 3,
+    return GestureDetector(
+      onTap: () => press(),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(2),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12),
+              border: Border.all(
+                color: HexColor("#6EBF8B"),
+                width: 3,
+              ),
+            ),
+            child: Hero(
+              tag: "${product.id}",
+              child: Image.network(
+                product.image,
+                height: 150,
+              ),
             ),
           ),
-          child: Image.network(
-            product.image,
-            height: 150,
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 0.5),
+            child: Text(
+              product.name,
+              style: const TextStyle(fontSize: 18),
+            ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 0.5),
-          child: Text(
-            product.name,
-            style: const TextStyle(fontSize: 18),
-          ),
-        ),
-        Text(
-          "\$${product.price}",
-          style: const TextStyle(fontWeight: FontWeight.bold),
-        )
-      ],
+          Text(
+            "\$${product.price}",
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          )
+        ],
+      ),
     );
   }
 }
+
