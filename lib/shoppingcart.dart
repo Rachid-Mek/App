7da0567fe88d25e4195931c96d78abd8ff1fe498
karimPrograms <78750964+karimPrograms@users.noreply.github.com>
2022-05-12 15:02:08 +0100

    Update shoppingcart.dart

--- lib/shoppingcart.dart
+++ lib/shoppingcart.dart
@@ -1,12 +1,17 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:my_app/others.dart';
+import 'package:app/med.dart';
+import 'package:app/others.dart';
+import 'globals.dart' as global;
 
 class shoppingcart extends StatefulWidget {
-  shoppingcart({Key? key}) : super(key: key);
+  List<Med> shoppinglist = List.empty();
+  shoppingcart({Key? key, required this.shoppinglist}) : super(key: key);
 
   @override
   State<shoppingcart> createState() => _shoppingcartState();
@@ -14,115 +19,157 @@ class shoppingcart extends StatefulWidget {
 
 class _shoppingcartState extends State<shoppingcart> {
   @override
+  void initState() {
+    super.initState();
+    total();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: HexColor("#EDE6DB"),
-      body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-              child: Text(
-                "Panier",
-                style: GoogleFonts.staatliches(
-                  textStyle: TextStyle(
-                    color: HexColor("#069A8E"),
-                    fontWeight: FontWeight.w400,
-                    fontSize: 48,
+    if (global.prixtotal != 0) {
+      return Scaffold(
+        backgroundColor: HexColor("#EDE6DB"),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                  child: Text(
+                    "Panier",
+                    style: GoogleFonts.staatliches(
+                      textStyle: TextStyle(
+                        color: HexColor("#069A8E"),
+                        fontWeight: FontWeight.w400,
+                        fontSize: 48,
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-            SizedBox(
-              height: 2,
-            ),
-            Container(
-              height: 375,
-              child: ListView(
-                scrollDirection: Axis.vertical,
-                children: [
-                  other(
-                      img: "images/doli500.jpg",
-                      name: "doliprane",
-                      mg: 500,
-                      qte: 10,
-                      price: 60),
-                  other(
-                      img: "images/doli500.jpg",
-                      name: "doliprane",
-                      mg: 500,
-                      qte: 10,
-                      price: 60),
-                  other(
-                      img: "images/doli500.jpg",
-                      name: "doliprane",
-                      mg: 500,
-                      qte: 10,
-                      price: 60),
-                  other(
-                      img: "images/doli500.jpg",
-                      name: "doliprane",
-                      mg: 500,
-                      qte: 10,
-                      price: 60),
-                ],
-              ),
-            ),
-            Spacer(),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                    child: Text(
-                      "Votre Total Est",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 25,
-                      ),
-                    ),
+                SizedBox(
+                  height: 2,
+                ),
+                Container(
+                  height: 375,
+                  child: ListView.builder(
+                    itemCount: widget.shoppinglist.length,
+                    itemBuilder: (context, index) => other(
+                        id: widget.shoppinglist[index].id,
+                        img: widget.shoppinglist[index].image,
+                        name: widget.shoppinglist[index].name,
+                        mg: widget.shoppinglist[index].Miligramme,
+                        qte: widget.shoppinglist[index].Qte,
+                        qteAchete: widget.shoppinglist[index].QteAchte,
+                        price: widget.shoppinglist[index].price),
+                    scrollDirection: Axis.vertical,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                    child: Text(
-                      "360 DA",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w300,
-                        fontSize: 25,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                    child: SizedBox(
-                      width: 165,
-                      height: 50,
-                      child: ElevatedButton(
-                        onPressed: (() {
-                          setState(
-                            () {},
-                          );
-                        }),
-                        child: const Text(
-                          "Résever",
+                ),
+                Spacer(),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                        child: Text(
+                          "Votre Total Est",
                           style: TextStyle(
-                            fontSize: 17,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 25,
                           ),
                         ),
-                        style: ElevatedButton.styleFrom(
-                            primary: HexColor("#069A8E"),
-                            shape: StadiumBorder()),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                        child: Text(
+                          "${global.prixtotal}",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                            fontSize: 25,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                        child: SizedBox(
+                          width: 165,
+                          height: 50,
+                          child: ElevatedButton(
+                            onPressed: (() {
+                              setState(
+                                () {},
+                              );
+                            }),
+                            child: const Text(
+                              "Résever",
+                              style: TextStyle(
+                                fontSize: 17,
+                              ),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                                primary: HexColor("#069A8E"),
+                                shape: StadiumBorder()),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      );
+    } else {
+      return Scaffold(
+        backgroundColor: HexColor("#EDE6DB"),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                  child: Text(
+                    "Panier",
+                    style: GoogleFonts.staatliches(
+                      textStyle: TextStyle(
+                        color: HexColor("#069A8E"),
+                        fontWeight: FontWeight.w400,
+                        fontSize: 48,
                       ),
                     ),
                   ),
-                ],
-              ),
-            )
-          ],
+                ),
+                SizedBox(
+                  height: 200,
+                ),
+                Text(
+                  "Votre Panier Est Vide",
+                  style: GoogleFonts.staatliches(
+                    textStyle: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 48,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
-      ),
-    );
+      );
+    }
   }
+}
+
+total() {
+  int prixprod = 0;
+  global.prixtotal = 0;
+  global.shoppinglist.forEach(
+    (element) {
+      prixprod = element.QteAchte * element.price;
+      global.prixtotal = global.prixtotal + prixprod;
+    },
+  );
 }
