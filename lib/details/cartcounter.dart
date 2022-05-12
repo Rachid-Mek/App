a22943c54a0610b0f97f375146fcd8cb5998548c
karimPrograms <78750964+karimPrograms@users.noreply.github.com>
2022-05-12 14:50:50 +0100

    Update cartcounter.dart

--- lib/details/cartcounter.dart
+++ lib/details/cartcounter.dart
@@ -1,53 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:app/globals.dart' as global;
 
 class CarteCounter extends StatefulWidget {
-  CarteCounter({Key? key}) : super(key: key);
+  int QteR;
+  final int id;
+  CarteCounter({Key? key, required this.QteR, required this.id})
+      : super(key: key);
 
   @override
   State<CarteCounter> createState() => _CarteCounterState();
 }
 
 class _CarteCounterState extends State<CarteCounter> {
-  int qte = 1;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Text(
-          "Quantité:",
-          style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Quantité:",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: <Widget>[
+                buildOutlinedButton(
+                  icon: Icons.remove,
+                  press: () {
+                    setState(() {
+                      if (global.qte > 1) {
+                        widget.QteR++;
+                        global.qte--;
+                      }
+                    });
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 7),
+                  child: Text(
+                    //if the qte is less than 10 it shown 01 02...
+                    global.qte.toString().padLeft(2, "0"),
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+                  ),
+                ),
+                buildOutlinedButton(
+                    icon: Icons.add,
+                    press: () {
+                      setState(() {
+                        if (widget.QteR > 0) {
+                          widget.QteR--;
+                          global.qte++;
+                        }
+                      });
+                    })
+              ],
+            ),
+          ],
         ),
-        SizedBox(
-          height: 10,
-        ),
-        Row(
-          children: <Widget>[
-            buildOutlinedButton(
-              icon: Icons.remove,
-              press: () {
-                setState(() {
-                  if (qte > 1) {
-                    qte--;
-                  }
-                });
-              },
+        Column(
+          children: [
+            Text(
+              "Quantité Restante:",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+            ),
+            SizedBox(
+              height: 12,
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 7),
               child: Text(
                 //if the qte is less than 10 it shown 01 02...
-                qte.toString().padLeft(2, "0"),
+                widget.QteR.toString().padLeft(2, "0"),
                 style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
               ),
             ),
-            buildOutlinedButton(
-                icon: Icons.add,
-                press: () {
-                  setState(() {
-                    qte++;
-                  });
-                })
           ],
         ),
       ],
