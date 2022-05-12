2bf1a51bd9acac18a373a0cb6b9ac769fad90fc6
karimPrograms <78750964+karimPrograms@users.noreply.github.com>
2022-05-12 15:03:15 +0100

    Add files via upload

---
+++ lib/globals.dart
@@ -0,0 +1,9 @@
+library globals;
+
+import 'package:app/med.dart';
+import 'package:app/others.dart';
+
+List<Med> shoppinglist = List.empty();
+int qte = 1;
+int prixtotal = 0;
+int qteRestante = 0;
---
+++ lib/med.dart
@@ -0,0 +1,15 @@
+class Med {
+  final int id, price, Miligramme;
+  int QteAchte, Qte;
+  final String image, name;
+
+  Med({
+    required this.id,
+    required this.Miligramme,
+    required this.name,
+    required this.Qte,
+    required this.price,
+    required this.image,
+    required this.QteAchte,
+  });
+}
