da83130e5cb6d06ea061f84e2d8616649264bf47
karimPrograms <78750964+karimPrograms@users.noreply.github.com>
2022-05-12 15:01:39 +0100

    Update product.dart

--- lib/product.dart
+++ lib/product.dart
@@ -1,53 +1,144 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
-import 'medicament.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 class ProductCard extends StatelessWidget {
-  final Product product;
+  final int id, price, Miligramme, Qte;
+  final String image, name;
   final Function press;
 
-  const ProductCard({Key? key, required this.product, required this.press})
-      : super(key: key);
+  const ProductCard({
+    Key? key,
+    required this.id,
+    required this.Miligramme,
+    required this.name,
+    required this.Qte,
+    required this.price,
+    required this.image,
+    required this.press,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => press(),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            padding: const EdgeInsets.all(2),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(12),
-              border: Border.all(
-                color: HexColor("#6EBF8B"),
-                width: 3,
+    if (Miligramme != 0) {
+      return GestureDetector(
+        onTap: () => press(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 150,
+              width: 150,
+              padding: const EdgeInsets.all(2),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12),
+                border: Border.all(
+                  color: HexColor("#6EBF8B"),
+                  width: 3,
+                ),
+              ),
+              child: Hero(
+                tag: "${id}",
+                child: Image.network(
+                  "https://pharmacile.000webhostapp.com/site/uploads/${image}",
+                  height: 150,
+                ),
               ),
             ),
-            child: Hero(
-              tag: "${product.id}",
-              child: Image.network(
-                product.image,
-                height: 150,
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 0.5),
+              child: Row(
+                children: [
+                  Text(
+                    name,
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                  SizedBox(
+                    width: 2,
+                  ),
+                  Text(
+                    "${Miligramme}",
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                ],
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 0.5),
-            child: Text(
-              product.name,
-              style: const TextStyle(fontSize: 18),
+            Container(
+              width: 150,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Quantité: ${Qte}",
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "\$${price}",
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      );
+    } else {
+      return GestureDetector(
+        onTap: () => press(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 150,
+              width: 150,
+              padding: const EdgeInsets.all(2),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12),
+                border: Border.all(
+                  color: HexColor("#6EBF8B"),
+                  width: 3,
+                ),
+              ),
+              child: Hero(
+                tag: "${id}",
+                child: Image.network(
+                  "https://pharmacile.000webhostapp.com/site/uploads/${image}",
+                  height: 150,
+                ),
+              ),
             ),
-          ),
-          Text(
-            "\$${product.price}",
-            style: const TextStyle(fontWeight: FontWeight.bold),
-          )
-        ],
-      ),
-    );
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 0.5),
+              child: Row(
+                children: [
+                  Text(
+                    name,
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: 150,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Quantité: ${Qte}",
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "\$${price}",
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      );
+    }
   }
 }
-
