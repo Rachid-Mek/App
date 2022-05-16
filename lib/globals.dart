b503d8dc342259ed3f7d543f043b2bc6ac5495d9
karimPrograms <78750964+karimPrograms@users.noreply.github.com>
2022-05-16 09:52:58 +0100

    Update globals.dart

--- lib/globals.dart
+++ lib/globals.dart
@@ -7,3 +7,7 @@ List<Med> shoppinglist = List.empty();
 int qte = 1;
 int prixtotal = 0;
 int qteRestante = 0;
+bool isloggedin = false;
+int userID = 0;
+List infouser = List.empty();
+bool reserve = false;
