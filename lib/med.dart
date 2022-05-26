aff5eaeb86fae83fb4d33b7ceaa59b19493ea2ef
karimPrograms <78750964+karimPrograms@users.noreply.github.com>
2022-05-26 14:56:03 +0100

    Update med.dart

--- lib/med.dart
+++ lib/med.dart
@@ -1,13 +1,14 @@
 class Med {
   final int id, price, Miligramme, idpharm;
   int QteAchte, Qte;
-  final String image, name;
+  final String image, name, pharmemail;
 
   Med({
     required this.id,
     required this.idpharm,
     required this.Miligramme,
     required this.name,
+    required this.pharmemail,
     required this.Qte,
     required this.price,
     required this.image,
