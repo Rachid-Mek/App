ad5a02a85e612d090159254181d0f169f9bcee5f
karimPrograms <78750964+karimPrograms@users.noreply.github.com>
2022-05-26 14:54:40 +0100

    Update globals.dart

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,13 +1,14 @@
 library globals;
 
 import 'package:app/med.dart';
-import 'package:app/others.dart';
 
 List<Med> shoppinglist = List.empty();
+String emailmessage = "";
 int qte = 1;
 int prixtotal = 0;
 int qteRestante = 0;
 bool isloggedin = false;
 int userID = 0;
 List infouser = List.empty();
+List globalproductList = List.empty();
 bool reserve = false;
