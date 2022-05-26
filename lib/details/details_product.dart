171ecde7f4690be68b7fd7f8d44a45cd3f513946
karimPrograms <78750964+karimPrograms@users.noreply.github.com>
2022-05-26 15:00:04 +0100

    Update details_product.dart

--- lib/details/details_product.dart
+++ lib/details/details_product.dart
@@ -1,13 +1,15 @@
 import 'package:app/details/body.dart';
 import 'package:app/main.dart';
-import 'package:app/shoppingcart.dart';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:app/globals.dart' as global;
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 class Details extends StatefulWidget {
   final int id, price, Miligramme, Qte, idpharm;
-  final String image, name;
+  final String image, name, pharmemail;
 
   const Details({
     Key? key,
@@ -15,6 +17,7 @@ class Details extends StatefulWidget {
     required this.idpharm,
     required this.Miligramme,
     required this.name,
+    required this.pharmemail,
     required this.Qte,
     required this.price,
     required this.image,
@@ -25,6 +28,39 @@ class Details extends StatefulWidget {
 }
 
 class _DetailsState extends State<Details> {
+  List Info = List.empty();
+  getUserInfo() async {
+    try {
+      var url =
+          "https://pharmacile.000webhostapp.com/appmobile/fetchpharminfo.php";
+      var data = {
+        "pharmID": widget.idpharm.toString(),
+      };
+      var response = await http.post(Uri.parse(url), body: data);
+      if (response.statusCode == 200) {
+        if (this.mounted) {
+          setState(
+            () {
+              Info = jsonDecode(response.body);
+            },
+          );
+        }
+        global.globalproductList = Info;
+        return (global.globalproductList);
+      }
+    } catch (e) {
+      EasyLoading.showError('Erreur de connexion');
+      EasyLoading.dismiss();
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getUserInfo();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,6 +74,7 @@ class _DetailsState extends State<Details> {
         Qte: widget.Qte,
         image: widget.image,
         name: widget.name,
+        pharmemail: widget.pharmemail,
       ),
     );
   }
@@ -52,7 +89,19 @@ class _DetailsState extends State<Details> {
       backgroundColor: HexColor("#6EBF8B"),
       elevation: 0,
       leading: IconButton(
-          onPressed: () => Navigator.pop(context),
+          onPressed: () {
+            setState(() {
+              global.qte = 1;
+              global.globalproductList = [];
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (BuildContext context) {
+                    return MainHome(pageindex: 0);
+                  },
+                ),
+              );
+            });
+          },
           icon: Image.asset('images/back.png')),
       title: !isSearching
           ? Text("")
